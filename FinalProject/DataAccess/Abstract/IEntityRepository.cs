57cad75e9dc30a1375b3229115580aa2cd9ccbd3
ipekozbay <20060401@stu.omu.edu.tr>
2022-03-01 23:37:59 +0300

    generic constraint 01.03.2022

--- FinalProject/DataAccess/Abstract/IEntityRepository.cs
+++ FinalProject/DataAccess/Abstract/IEntityRepository.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using Entities.Abstract;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    // generic constraint
+    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         List<T> GetAll(Expression <Func<T,bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);
