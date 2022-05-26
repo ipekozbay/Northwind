58bde8e01f7335f10694fbea1e8e34e43dde93dd
ipekozbay <20060401@stu.omu.edu.tr>
2022-05-26 19:08:10 +0300

    26.05.2022

--- FinalProject/Business/Concrete/ProductManager.cs
+++ FinalProject/Business/Concrete/ProductManager.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Business.Abstract;
 using DataAccess.Abstract;
-using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
-
 namespace Business.Concrete
 {
     public class ProductManager : IProductService
@@ -21,6 +16,16 @@ namespace Business.Concrete
         public List<Product> GetAll()
         {
             return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategory(int id)
+        {
+            return _productDal.GetAll(p=>p.CategoryId == id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice<=max);
         }
     }
 }
